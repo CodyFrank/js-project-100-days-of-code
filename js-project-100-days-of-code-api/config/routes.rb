da8750827e4b32d56c3a59245c64b3ec6c00d97c
Cody <cody.frank30@gmail.com>
2019-12-09 18:44:40 -0500

    updated routes for namespaced controllers

--- js-project-100-days-of-code-api/config/routes.rb
+++ js-project-100-days-of-code-api/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :challenges
-  resources :days
+  namespace :api do
+    namespace :v1 do
+      resources :challenges
+      resources :days
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
