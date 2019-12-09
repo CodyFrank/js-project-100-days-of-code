2e84ba541391afd687119dec51ea98c289bb5918
Cody <cody.frank30@gmail.com>
2019-12-09 17:55:45 -0500

    added strong params to challenge controller

--- js-project-100-days-of-code-api/app/controllers/api/v1/challenges_controller.rb
+++ js-project-100-days-of-code-api/app/controllers/api/v1/challenges_controller.rb
@@ -20,4 +20,10 @@ class Api::V1::ChallengesController < ApplicationController
     def destroy
     end
 
+    private
+
+    def challenge_params
+        params.require(:challenge).permit(:question, :solution, :description)
+    end
+
 end
