afe26c3b3dcd5441fb3dec2790edf833bdc3d72e
Cody <cody.frank30@gmail.com>
2019-12-09 21:10:28 -0500

    created days adapter and getDays method to fetch days

---
+++ frontend/src/adapters/DayAdapter.js
@@ -0,0 +1,11 @@
+class DayAdapter{
+
+    constructor(){
+        this.baseUrl = "http://localhost:3000/api/v1/days"
+    }
+
+    getDays() {
+        return fetch(this.baseUrl).then(res => res.json())
+    }
+
+}
