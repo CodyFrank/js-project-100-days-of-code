65e03438e07d7d2bc291f08088e77cfbb621db2c
Cody <cody.frank30@gmail.com>
2019-12-09 19:45:08 -0500

    added options method to the days controller as an option for serializer instantiation

--- js-project-100-days-of-code-api/app/controllers/api/v1/days_controller.rb
+++ js-project-100-days-of-code-api/app/controllers/api/v1/days_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::DaysController < ApplicationController
     def index
         @days = Day.all
-        render json: DaySerializer.new(@days)
+        render json: DaySerializer.new(@days, options)
     end
 
     def show
         @day = Day.find(day_params[:id])
-        render json: DaySerializer.new(@day)
+        render json: DaySerializer.new(@day, options)
     end
 
     def create
         @day = Day.new(day_params)
         if @day.save
-            render json: DaySerializer.new(@day)
+            render json: DaySerializer.new(@day, options)
         else
             #throw some error here
         end
@@ -21,7 +21,7 @@ class Api::V1::DaysController < ApplicationController
     def update
         @day = Day.find(day_params[:id])
         if @day.update(day_params)
-            render json: DaySerializer.new(@day)
+            render json: DaySerializer.new(@day, options)
         else
             #throw some error here
         end
@@ -36,6 +36,12 @@ class Api::V1::DaysController < ApplicationController
 
     def day_params
         params.require(:day).permit(:id, :date)
+    end
+
+    def options
+        {
+            include: [:challenges]
+        }
     end
 
 end
