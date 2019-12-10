3c5943483e61d1119de70c868d5cf3cd9adff9d9
Cody <cody.frank30@gmail.com>
2019-12-09 21:45:44 -0500

    created days fetchAndLoadDays

--- frontend/src/components/days.js
+++ frontend/src/components/days.js
@@ -2,7 +2,11 @@ class Days{
     constructor(){
         this.days = []
         this.adapter = new DaysAdapter()
-        this.bindEventListeners()
+        // this.bindEventListeners()
         this.fetchAndLoadDays()
+    }
+
+    fetchAndLoadDays(){
+        this.adapter.getDays().then(days => console.log(days))
     }
 }
