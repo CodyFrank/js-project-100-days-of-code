8a7f1b17fdc57852847b1e7a87e9559d1e997b5f
Cody <cody.frank30@gmail.com>
2019-12-17 18:12:31 -0500

    removed instance variables and serializer instantiation from days controller

--- js-project-100-days-of-code-api/app/controllers/api/v1/days_controller.rb
+++ js-project-100-days-of-code-api/app/controllers/api/v1/days_controller.rb
@@ -1,27 +1,27 @@
 class Api::V1::DaysController < ApplicationController
     def index
-        @days = Day.all
-        render json: DaySerializer.new(@days, options)
+        days = Day.all
+        render json: days
     end
 
     def show
-        @day = Day.find(params[:id])
-        render json: DaySerializer.new(@day, options)
+        day = Day.find(params[:id])
+        render json: day
     end
 
     def create
-        @day = Day.new(params)
-        if @day.save
-            render json: DaySerializer.new(@day, options)
+        day = Day.new(params)
+        if day.save
+            render json: day
         else
             #throw some error here
         end
     end
 
     def update
-        @day = Day.find(params[:id])
-        if @day.update(params)
-            render json: DaySerializer.new(@day, options)
+        day = Day.find(params[:id])
+        if day.update(params)
+            render json: day
         else
             #throw some error here
         end
