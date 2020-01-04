eebabeabe8a2ae6f1e69484cd57663b980fb44d8
Cody <cody.frank30@gmail.com>
2020-01-04 14:18:25 -0500

    created 3 new render methods in the day class to render that days challenges properties

--- frontend/src/components/day.js
+++ frontend/src/components/day.js
@@ -11,14 +11,31 @@ class Day{
         this.daysContainer = document.querySelector('div.days-container')
     }
 
+    renderChallengeQuestion(){
+        return this.challenges.map(c => c.question)
+    }
+
+    renderChallengeDescription(){
+        return this.challenges.map(c => c.description)
+    }
+
+    renderChallengeSolution(){
+        return this.challenges.map(c => c.Solution)
+    }
+
     renderDay(){
         return `<button class="collapsible day-button" data-id='${this.id}' id='dateid${this.id}'>${this.date}</button>
         <div class="challenges">
-          <p>put a challenge here</p>
-          <p>put a solution here</p>
-          <p>put a description here</p>
+        <p>${this.renderChallengeQuestion()}<p>
+        <p>${this.renderChallengeDescription()}<p>
+        <p>${this.renderChallengeSolution()}<p>
           <button class='delete-button' data-id=${this.id}>Delete</button>
         </div>`
     }
 }
+
+
+{/* <p>put a challenge here</p>
+<p>put a solution here</p>
+<p>put a description here</p> */}
 
